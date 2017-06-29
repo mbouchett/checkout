e4885493b06fcb08ff412fb3749fd3c12d73aa36
mbouchett <mark@markbouchett.com>
2017-06-28 21:59:04 -0400

    MyStack Header Just Getting Started

---
+++ checkout/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: Mark Bouchett
+ *
+ * Created on June 28, 2017, 9:57 PM
+ */
+
+#include <iostream>  //console Input/Output
+#include <string>  //string handler
+
+using namespace std; //declares the name space
+// declare function prototypes
+
+/*
+ * 
+ */
+int main() {
+
+    return 0;
+}
+
