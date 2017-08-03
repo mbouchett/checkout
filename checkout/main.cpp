c24a9ee548b8aa1be6274dd153100d9ae8ddc5b0
mbouchett <mark@markbouchett.com>
2017-08-02 21:11:10 -0400

    shaping the ime keeper -mb

--- checkout/main.cpp
+++ checkout/main.cpp
@@ -5,12 +5,24 @@ using namespace std;
 
 int main(void)
 {
-    int const SIMTIME = 60;        //Simulation runtime in seconds 900 = 15min
+    int const SIMTIME = 480;        //Simulation runtime in seconds 900 = 15min
     time_t timeBeg = time(0);       //used for the timer
     time_t timeEnd = time(0);       //used for the timer
+    int elapsedSecs = timeEnd-timeBeg;
+    int oldSecs = elapsedSecs;
+    int hour = 10;
     
-    while((timeEnd-timeBeg) <= SIMTIME){
-        
+    cout << "The store is open!\n------------------\n";
+    cout << "The time is now " << hour <<":00\n";
+    
+    while(elapsedSecs <= SIMTIME){
+        elapsedSecs = timeEnd-timeBeg;
+        if(elapsedSecs > oldSecs && (elapsedSecs % 60) == 0){
+            //one minute real time / one hour sim time has passed
+            oldSecs = elapsedSecs;
+            hour++;
+            cout << "The time is now " << hour <<":00\n"; 
+        }
         
         timeEnd = time(0);
     };
